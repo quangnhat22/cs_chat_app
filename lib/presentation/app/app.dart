4f9acd538b94bf4845572faf2515f60c90c081b2
nhatquangnguyendinh <wuangsotech@gmail.com>
2023-04-21 10:31:42 +0700

    intial home

--- lib/presentation/app/app.dart
+++ lib/presentation/app/app.dart
@@ -1,9 +1,10 @@
 import 'package:chatapp/common/widgets/stateless/m_material_app.dart';
 import 'package:chatapp/presentation/app/bloc/app_bloc.dart';
-import 'package:chatapp/routes/app_routes.dart';
-import 'package:chatapp/routes/route_name.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+
+import '../../core/routes/app_routes.dart';
+import '../../core/routes/route_name.dart';
 
 class App extends StatelessWidget {
   const App({Key? key}) : super(key: key);
