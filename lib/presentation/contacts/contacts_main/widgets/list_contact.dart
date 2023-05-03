652e61cd5ae8d61f2f64ae4a0ac4e78db3c03162
LeHaGiaBao <20520141@gm.uit.edu.vn>
2023-05-03 22:00:45 +0700

    Add contact page

---
+++ lib/presentation/contacts/contacts_main/widgets/list_contact.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class ListContact extends StatelessWidget {
+  const ListContact({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text('List Contact'),
+    );
+  }
+}
