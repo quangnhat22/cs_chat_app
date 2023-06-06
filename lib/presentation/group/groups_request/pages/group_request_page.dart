2985ce8e2c8af758f4b7a6d262fad61566455861
LeHaGiaBao <20520141@gm.uit.edu.vn>
2023-06-06 15:03:10 +0700

    Update group request page

--- lib/presentation/group/groups_request/pages/group_request_page.dart
+++ lib/presentation/group/groups_request/pages/group_request_page.dart
@@ -1,7 +1,11 @@
-import 'package:chatapp/presentation/group/groups_request/widgets/group_received_request_list.dart';
-import 'package:chatapp/presentation/group/groups_request/widgets/group_requests_segmented_button.dart';
-import 'package:chatapp/presentation/group/groups_request/widgets/group_sent_request_list.dart';
+import 'package:chatapp/core/di/injector.dart';
+import 'package:chatapp/core/utils/snack_bar.dart';
+import 'package:chatapp/presentation/friends/friends_request/friends_request.dart';
+import 'package:chatapp/presentation/group/groups_request/bloc/list_group_request_bloc.dart';
+import 'package:chatapp/presentation/group/groups_request/cubit/group_request_action_cubit.dart';
+import 'package:chatapp/presentation/group/groups_request/widgets/segment_button_group_request.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 enum Request { sent, received }
 
@@ -10,7 +14,18 @@ class GroupRequestPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const GroupRequestView();
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(
+          create: (_) => getIt<ListGroupRequestBloc>()
+            ..add(const ListGroupRequestEvent.started()),
+        ),
+        BlocProvider(
+          create: (_) => getIt<GroupRequestActionCubit>(),
+        ),
+      ],
+      child: const GroupRequestView(),
+    );
   }
 }
 
@@ -32,22 +47,68 @@ class _GroupRequestViewState extends State<GroupRequestView> {
 
   @override
   Widget build(BuildContext context) {
-    return SingleChildScrollView(
-        child: Column(
-      children: <Widget>[
-        Padding(
-          padding: const EdgeInsets.only(left: 20, right: 20, top: 16),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              GroupRequestsSegmentedButton(handleViewChange),
-            ],
-          ),
-        ),
-        view == Request.sent
-            ? const GroupSentRequestList()
-            : const GroupReceivedRequestList()
-      ],
-    ));
+    return BlocListener<GroupRequestActionCubit, GroupRequestActionState>(
+      listenWhen: (previous, current) => previous != current,
+      listener: (context, state) {
+        state.whenOrNull(
+            failure: (message) =>
+                SnackBarApp.showSnackBar(context, message, TypesSnackBar.error),
+            success: () {
+              context
+                  .read<ListGroupRequestBloc>()
+                  .add(const ListGroupRequestRefreshed());
+              SnackBarApp.showSnackBar(
+                  context, "Success", TypesSnackBar.success);
+            });
+      },
+      child: BlocBuilder<ListGroupRequestBloc, ListGroupRequestState>(
+        builder: (context, state) {
+          return state.maybeWhen(
+              getDataSuccess: (groupRequestSent, groupRequestReceive) {
+            return SingleChildScrollView(
+              child: RefreshIndicator(
+                onRefresh: () async {
+                  context
+                      .read<ListGroupRequestBloc>()
+                      .add(const ListGroupRequestEvent.listRequestRefreshed());
+                },
+                child: Column(
+                  children: <Widget>[
+                    Padding(
+                      padding: const EdgeInsets.only(
+                        left: 20,
+                        right: 20,
+                        top: 16,
+                        bottom: 8,
+                      ),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: <Widget>[
+                          SegmentButtonGroupRequest(handleViewChange)
+                        ],
+                      ),
+                    ),
+                    // TODO: Fix bug type The argument type 'List<GroupRequestEntity>' can't be assigned to the parameter
+                    // view == Request.sent
+                    //     ? ListRequestFriendSend(
+                    //         listSentRequest: groupRequestSent,
+                    //       )
+                    //     : ListRequestFriendReceive(
+                    //         listReceiveRequest: groupRequestReceive,
+                    //       ),
+                  ],
+                ),
+              ),
+            );
+          }, getDataFail: (_) {
+            return const SomeThingWrong();
+          }, orElse: () {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          });
+        },
+      ),
+    );
   }
 }
